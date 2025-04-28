c998c8202e80fcb7952e054acab29788c8602063
techie007 <11600782+dpo007@users.noreply.github.com>
2025-04-27 20:42:29 -0400

    Increase minimum escape path length in dungeon settings
    Updated the `DefaultEscapePathLength` property from 10 to 15, enhancing the minimum number of rooms required from the Entrance to Exit in the dungeon.

--- DynDungeonCrawler.Engine/Constants/DungeonDefaults.cs
+++ DynDungeonCrawler.Engine/Constants/DungeonDefaults.cs
@@ -3,7 +3,7 @@ namespace DynDungeonCrawler.Engine.Constants
     public static class DungeonDefaults
     {
         public static string DefaultDungeonDescription { get; } = "A D&D style labyrinth filled with ancient secrets.";
-        public static int DefaultEscapePathLength { get; } = 10; // Minimum rooms from Entrance to Exit
+        public static int DefaultEscapePathLength { get; } = 15; // Minimum rooms from Entrance to Exit
         public static int MaxDungeonWidth { get; } = 100; // Maximum width of the dungeon
         public static int MaxDungeonHeight { get; } = 100; // Maximum height of the dungeon
     }
